ef9dc6b289fc12f3afc1a38d75e3e21be9946a9c
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-06 21:45:51 +0400

    Boolean Conditions

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first >= second) {
+            System.out.println(first);
+        } else
+            System.out.println(second);
+    }
+
+    public static void main(String[] args) {
+        GreatestNumberPrinter greatestNumberPrinter = new GreatestNumberPrinter();
+        greatestNumberPrinter.printGreatest(10, -5);
+        greatestNumberPrinter.printGreatest(5, 80);
+        greatestNumberPrinter.printGreatest(9, 9);
     }
 }
